762e25a969a9fde0329b3a121be7466ae3699cec
maarten-jan <maarten-jan.van.gool@praegus.nl>
2019-05-06 10:56:50 +0200

    fix test

--- src/test/java/nl/praegus/fitnesse/slim/fixtures/ImageCompareFixtureTest.java
+++ src/test/java/nl/praegus/fitnesse/slim/fixtures/ImageCompareFixtureTest.java
@@ -9,14 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ImageCompareFixtureTest {
 
     @Test
-    public void when_pictueres_are_compared_a_comparison_map_is_created() throws IOException {
+    public void when_pictures_are_compared_a_comparison_map_is_created() throws IOException {
         ImageCompareFixture imageCompareFixture = new ImageCompareFixture();
 
         String result = imageCompareFixture.differencesBetweenAnd("src/test/resources/praegus-p.png", "src/test/resources/praegus-p.png");
         assertThat(result).contains("<div><a href=\"files/comparison-map-");
-        assertThat(result).contains(".png\" target=\"_blank\"><img src=\"files/comparison-map-");
-        assertThat(result).contains(".png\" title=\"toolchain-fixtures/FitNesseRoot/files/comparison-map-");
-        assertThat(result).contains(".png\" height=\"400\"/></a></div>");
+        assertThat(result).contains("target=\"_blank\"><img src=\"files/comparison-map-");
+        assertThat(result).contains("title=\"toolchain-fixtures/FitNesseRoot/files/comparison-map-");
+        assertThat(result).contains("height=\"400\"/></a></div>");
 
 
     }
