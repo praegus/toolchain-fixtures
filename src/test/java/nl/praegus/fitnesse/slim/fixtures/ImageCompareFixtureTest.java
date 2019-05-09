a7bab71da0bcf7acc33f38cd3ed062a9b0281f2c
Tom Heintzberger <tom.heintzberger@gmail.com>
2019-05-09 13:46:11 +0200

    make test windows-separator-proof

--- src/test/java/nl/praegus/fitnesse/slim/fixtures/ImageCompareFixtureTest.java
+++ src/test/java/nl/praegus/fitnesse/slim/fixtures/ImageCompareFixtureTest.java
@@ -2,6 +2,7 @@ package nl.praegus.fitnesse.slim.fixtures;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,7 +16,7 @@ public class ImageCompareFixtureTest {
         String result = imageCompareFixture.differencesBetweenAnd("src/test/resources/praegus-p.png", "src/test/resources/praegus-p.png");
         assertThat(result).contains("<div><a href=\"files/comparison-map-");
         assertThat(result).contains("target=\"_blank\"><img src=\"files/comparison-map-");
-        assertThat(result).contains("toolchain-fixtures/FitNesseRoot/files/comparison-map-");
+        assertThat(result).contains("toolchain-fixtures/FitNesseRoot/files/comparison-map-".replace("/", File.separator));
         assertThat(result).contains("height=\"400\"/></a></div>");
 
 
