ad88addc5dccb1e26a67a55a95b290d0cab982af
Tom Heintzberger <tom.heintzberger@gmail.com>
2019-05-13 13:39:45 +0200

    Let mendix browser test wait for any progress indicator; fix npe when table row could not be found

--- src/main/java/nl/praegus/fitnesse/slim/fixtures/web/mendix/MendixBrowserTest.java
+++ src/main/java/nl/praegus/fitnesse/slim/fixtures/web/mendix/MendixBrowserTest.java
@@ -1,22 +1,107 @@
 package nl.praegus.fitnesse.slim.fixtures.web.mendix;
 
+import nl.hsac.fitnesse.fixture.slim.SlimFixtureException;
 import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.TimeoutPolicy;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.WaitUntil;
 import nl.hsac.fitnesse.fixture.util.selenium.SeleniumHelper;
+import nl.hsac.fitnesse.slim.interaction.ReflectionHelper;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+
+import java.lang.reflect.Method;
+import java.util.Set;
 
 /**
  * Fixture class customized to test mendix web apps.
  */
 
 public class MendixBrowserTest extends BrowserTest<WebElement> {
+    private final static Set<String> METHODS_NO_WAIT;
+    static {
+        METHODS_NO_WAIT = ReflectionHelper.validateMethodNames(BrowserTest.class,
+                "open",
+                "takeScreenshot",
+                "location",
+                "back",
+                "forward",
+                "refresh",
+                "alertText",
+                "confirmAlert",
+                "dismissAlert",
+                "openInNewTab",
+                "ensureActiveTabIsNotClosed",
+                "currentTabIndex",
+                "tabCount",
+                "ensureOnlyOneTab",
+                "closeTab",
+                "switchToNextTab",
+                "switchToPreviousTab",
+                "switchToDefaultContent",
+                "switchToFrame",
+                "switchToParentFrame",
+                "secondsBeforeTimeout",
+                "secondsBeforePageLoadTimeout",
+                "waitForPage",
+                "waitForTagWithText",
+                "waitForClassWithText",
+                "waitForClass",
+                "waitForVisible",
+                "waitSeconds",
+                "waitMilliseconds",
+                "waitMilliSecondAfterScroll",
+                "screenshotBaseDirectory",
+                "screenshotShowHeight",
+                "setBrowserWidth",
+                "setBrowserHeight",
+                "setBrowserSizeToBy",
+                "setBrowserSizeToMaximum",
+                "setGlobalValueTo",
+                "setSendCommandForControlOnMacTo",
+                "sendCommandForControlOnMac",
+                "isImplicitWaitForAngularEnabled",
+                "setImplicitWaitForAngularTo",
+                "globalValue",
+                "clearSearchContext",
+                "executeScript",
+                "secondsBeforeTimeout",
+                "secondsBeforePageLoadTimeout",
+                "trimOnNormalize",
+                "setImplicitFindInFramesTo",
+                "setTrimOnNormalize",
+                "setRepeatIntervalToMilliseconds",
+                "repeatAtMostTimes",
+                "repeatAtMostTimes",
+                "timeSpentRepeating");
+    }
+
     private boolean waitForJquery = false;
     private int jqueryTimeout = 5; //Timeout in seconds
     private SeleniumHelper seleniumHelper = getEnvironment().getSeleniumHelper();
     private int delayBeforeValue = 0;
     private int inputDelay = 0;
+
+
+    /**
+     * Override beforeInvoke to always wait for any spinner to be gone if it's there
+     */
+    @Override
+    protected void beforeInvoke(Method method, Object[] arguments) {
+        super.beforeInvoke(method, arguments);
+        waitForJqueryIfNeeded();
+        if (!METHODS_NO_WAIT.contains(method.getName())) {
+            try {
+                waitUntil(webDriver -> isNotVisibleOnPage("css=.mx-progress-indicator"));
+            } catch (SlimFixtureException e) {
+                String msg = e.getMessage();
+                if (msg.startsWith("message:<<") && msg.endsWith(">>")) {
+                    msg = msg.substring(10, msg.length() - 2);
+                    msg = msg.replaceAll("Timed-out waiting ", "Timed out waiting for spinner to disappear ");
+                }
+                throw new SlimFixtureException(false, msg);
+            }
+        }
+    }
 
     public void waitForJquery(boolean wait) {
         waitForJquery = wait;
@@ -108,13 +193,16 @@ public class MendixBrowserTest extends BrowserTest<WebElement> {
 
     @WaitUntil(TimeoutPolicy.RETURN_NULL)
     public String valueOfInRowWhereIs(String requestedColumnName, String selectOnColumn, String selectOnValue) {
-        waitForJqueryIfNeeded();
+
         WebElement element = elementInRowWhereIs(requestedColumnName, selectOnColumn, selectOnValue);
-        String result = valueFor(element);
-        if (result.length() == 0) {
-            result = element.getAttribute("title");
+        if(element != null) {
+            String result = valueFor(element);
+            if (result.length() == 0) {
+                result = element.getAttribute("title");
+            }
+            return result;
         }
-        return result;
+        return null;
     }
 
     @Override
@@ -131,7 +219,7 @@ public class MendixBrowserTest extends BrowserTest<WebElement> {
         int requestedColumnIndex = columnIndex(xpathForHeadTable, requestedColumnName);
 
         String xpath = String.format("%s//td[%s][contains(.,'%s')]/parent::tr/td[%s]", xpathForBodyTable, selectColumnIndex, selectOnValue, requestedColumnIndex);
-        waitForJqueryIfNeeded();
+
         return seleniumHelper.findByXPath(xpath);
     }
 
@@ -142,7 +230,6 @@ public class MendixBrowserTest extends BrowserTest<WebElement> {
         WebElement el;
         while (!displayed) {
             xpath = String.format("(//*[text()='%s']/ancestor::div[@class='row']//table[contains(@class, '%s-table')])[%s]", table, type, String.valueOf(i));
-            waitForJqueryIfNeeded();
             el = seleniumHelper.findByXPath(xpath);
             if (el == null) {
                 xpath = String.format("//*[text()='%s']/ancestor::div[@class='row']//table[contains(@class, '%s-table')]", table, type);
@@ -182,17 +269,6 @@ public class MendixBrowserTest extends BrowserTest<WebElement> {
         return result;
     }
 
-    @Override
-    protected WebElement getElement(String place, String container){
-        waitForJqueryIfNeeded();
-        return super.getElement(place, container);
-    }
-
-    @Override
-    protected WebElement getElementToClick(String place, String container) {
-        waitForJqueryIfNeeded();
-        return super.getElementToClick(place, container);
-    }
 
     private void waitForJqueryIfNeeded() {
         if(waitForJquery) {
@@ -205,12 +281,13 @@ public class MendixBrowserTest extends BrowserTest<WebElement> {
 
                 secondsBeforeTimeout(originalTimeout);
             } catch (WebDriverException e) {
-                System.err.println("Exception when checking jquery status. Continue.");
+                // ignore. Probably no jQ.
             }
         }
     }
 
     private boolean jQueryIsDone() {
-        return (Boolean) executeScript("return window.jQuery.active == 0");
+        boolean result = (Boolean) executeScript("return window.jQuery.active === 0");
+        return result;
     }
 }
