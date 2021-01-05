961a6729a77b32a477b0efa471ab319043a95a3e
lennyg1 <30591999+lennyg1@users.noreply.github.com>
2021-01-05 11:34:56 +0100

    First steps

---
+++ src/main/java/nl/praegus/fitnesse/slim/fixtures/web/praegus/PraegusBrowserTest.java
@@ -0,0 +1,13 @@
+package nl.praegus.fitnesse.slim.fixtures.web.praegus;
+
+import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
+import org.openqa.selenium.*;
+
+/**
+ * Custom Praegus fixture class
+ */
+
+public class PraegusBrowserTest extends BrowserTest<WebElement> {
+
+
+}
